1056d37edecec23651a3f9bab9f2ae5d639c0b88
freewil4free <wilzeke@gmail.com>
2025-08-03 16:22:28 +1000

    updated loading functions and refined source data

--- load_data.js
+++ load_data.js
@@ -3,7 +3,7 @@ async function loadCSVData() {
     const loadingDiv = document.getElementById("loadingMessage");
     loadingDiv.style.display = 'block';
 
-    const response = await fetch("combined_data.csv");
+    const response = await fetch("refined_data.csv");
     const csvText = await response.text();
 
     return new Promise((resolve, reject) => {
@@ -18,16 +18,22 @@ async function loadCSVData() {
 
             rows.forEach(cols => {
                 const tr = document.createElement("tr");
+                const skipindex = [5,6,7]; // define the columns to skip appending (index starts at 0)
 
                 cols.forEach((col, index) => {
                     const td = document.createElement("td");
+                    
                     if (index === 2) {
                         const a = document.createElement("a");
-                        a.href = cols[6];
-                        a.textContent = cols[2];
+                        a.href = cols[7]; // append the value in the URL column to the value in the product title column
+                        a.textContent = cols[2]+" ($"+cols[5]+"/"+cols[6]+")";
                         a.target = "_blank";
                         td.appendChild(a);
-                    } else {
+                    } 
+                    else if (skipindex.includes(index)) { // skip the columns specified in skipindex
+                        return;
+                    } 
+                    else {
                         td.textContent = col.trim();
                     }
                     tr.appendChild(td);
@@ -47,3 +53,4 @@ async function loadCSVData() {
         };
     });
 }
+const skipIndexes = [1, 3]; // Skip 2nd and 4th columns
